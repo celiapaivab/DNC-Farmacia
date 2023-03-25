b815dc4715488935c5acd106a95de8c6e644630e
celiapaivab <126210304+celiapaivab@users.noreply.github.com>
2023-03-25 11:19:31 -0500

    Add files via upload
    Primeiro versão do formulário da farmácia.

---
+++ script.js
@@ -0,0 +1,47 @@
+let meds = [];
+
+function addMed() {
+    event.preventDefault();
+
+    let med = {
+        "name": document.getElementById('medName').value,
+        "quant": document.getElementById('medQuant').value,
+        "classe": document.getElementById('medClass').value,
+    }
+
+    document.getElementById('medName').value = "";
+    document.getElementById('medQuant').value = "";
+    document.getElementById('medClass').value = "";
+
+    meds.push(med);
+
+    renderTable();
+}
+
+function renderTable() {
+
+    let table = document.getElementById('tabela');
+    table.innerHTML = ` <tr>
+    <th> ID </th>
+    <th> Medicamento </th>
+    <th> Quantidade </th>
+    <th> Classe de Medicamento </th>
+    <th style="background-color: rgba(62, 61, 61, 0.75);"> Remover </th>
+</tr>`;
+
+    for (let i = 0; i < meds.length; i++) {
+
+        table.innerHTML = table.innerHTML + `<td> ${i} </td>
+    <td> ${meds[i].medName} </td>
+    <td> ${meds[i].medQuant} </td>
+    <td> ${meds[i].medClass} </td>
+    <td><img src="remove.svg" width="25px" height="25px" onclick="removeMed(${i})"></td>`
+    }
+
+}
+
+function removeMed(index) {
+
+    meds.splice(index, 1);
+    renderTable();
+}
